06a6e8ab3598a4282b4b2d2700d678a77aa4f4ad
alexander.galkin <alexander.galkin@corp.mail.ru>
2019-03-18 22:26:28 +0300

    Initial commit

---
+++ src/main/java/ru/spsuace/homework4/streams/CopyFile.java
@@ -0,0 +1,20 @@
+package ru.spsuace.homework4.streams;
+
+import java.io.IOException;
+
+public class CopyFile {
+
+    /**
+     * Реализовать копирование маленьких файлов, через чтение и запись всех строк разом.
+     * Если не получилось скопировать файл, то вернуть причину. Если получилось - вернуть "Ok"
+     */
+    public static String copySmallFiles(String pathFrom, String pathTo) {
+        return null;
+    }
+
+    /**
+     * Реализовать копирование больших файлов.
+     */
+    public static void copyBigFiles(String path) throws IOException {
+    }
+}
