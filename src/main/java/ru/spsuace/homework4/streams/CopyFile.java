f553821d29d89db4e0e45ca909d0e542a85332ed
Valeriy Smolskiy <smolskiy_pmi@mail.ru>
2019-03-23 22:06:24 +0300

    CopySmallFiles

--- src/main/java/ru/spsuace/homework4/streams/CopyFile.java
+++ src/main/java/ru/spsuace/homework4/streams/CopyFile.java
@@ -1,6 +1,9 @@
 package ru.spsuace.homework4.streams;
 
-import java.io.IOException;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class CopyFile {
 
@@ -8,13 +11,38 @@ public class CopyFile {
      * Реализовать копирование маленьких файлов, через чтение и запись всех строк разом.
      * Если не получилось скопировать файл, то вернуть причину. Если получилось - вернуть "Ok"
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+    public static String copySmallFiles(String pathFrom, String pathTo) throws Exception {
+
+        String allString = "";
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(pathFrom), "Cp1251"))) {
+            String str;
+
+            while ((str = br.readLine()) != null) {
+                allString = allString + str + "\n";
+            }
+            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(pathTo), "Cp1251"))) {
+
+                bw.write(allString);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return allString;
     }
+
 
     /**
      * Реализовать копирование больших файлов.
      */
     public static void copyBigFiles(String path) throws IOException {
     }
+
+    public static void main(String[] arg) throws Exception {
+        copySmallFiles("E:\\test.txt", "E:\\test1.txt");
+        //System.out.println;
+    }
 }
+
+
