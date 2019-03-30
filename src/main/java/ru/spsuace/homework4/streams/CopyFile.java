2a4db9ebac7df42cc13abe78811f2afeadb66a7c
Polina Shirobokova <izhevsk07@gmail.com>
2019-03-30 22:06:51 +0300

    задание CopyFile

--- src/main/java/ru/spsuace/homework4/streams/CopyFile.java
+++ src/main/java/ru/spsuace/homework4/streams/CopyFile.java
@@ -1,6 +1,14 @@
 package ru.spsuace.homework4.streams;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Для этого задания надо использовать только новый API (нельзя использовать класс File)
@@ -13,7 +21,17 @@ public class CopyFile {
      * Для маленьких фалов есть отличный стрим для этой задачи
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+        String answer = "";
+        try {
+            List<String> text = Files.readAllLines(Paths.get(pathFrom));
+            Path path = Paths.get(pathTo);
+            Files.write(path, text, StandardCharsets.UTF_8);
+            answer = "ok";
+        } catch (IOException e) {
+            answer = "Something gone wrong..";
+        } finally {
+            return answer;
+        }
     }
 
     /**
@@ -21,5 +39,16 @@ public class CopyFile {
      * через стримы. Класс для стримов из верхнего задания использовать нельзя
      */
     public static void copyBigFiles(String pathFrom, String pathTo) throws IOException {
+
+        FileReader fileReader = new FileReader(pathFrom);
+        FileWriter fileWriter = new FileWriter(pathTo);
+        Scanner scanner = new Scanner(fileReader);
+
+        while (scanner.hasNextLine()) {
+            fileWriter.write(scanner.nextLine());
+        }
+
+        fileReader.close();
+        fileWriter.close();
     }
 }
