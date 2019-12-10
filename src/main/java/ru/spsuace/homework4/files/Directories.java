bd344dfc93e08344381d92f1a0022de5f2239ed7
MarinaShakhova1946 <shak.marina15@gmail.com>
2019-12-10 23:30:31 +0300

    HomeWork 4

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,5 +1,12 @@
 package ru.spsuace.homework4.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Directories {
 
 
@@ -10,13 +17,48 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        int count = 0;
+        File main = new File(path);
+        if (!main.exists()) {
+            return count;
+        }
+        File[] listFiles = main.listFiles();
+        if (listFiles != null) {
+            for (int i = 0; i < listFiles.length; i++) {
+                if (listFiles[i].isFile()) {
+                    listFiles[i].delete();
+                    count += 1;
+                }
+                if (listFiles[i].isDirectory()) {
+                    count += removeWithFile(listFiles[i].getPath());
+                }
+            }
+        }
+        main.delete();
+        count += 1;
+        return count;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        final Path file = Paths.get(path);
+        if (!file.toFile().exists()) {
+            return 0;
+        }
+        return deletePath(file) + 1;
+    }
+
+    private static int deletePath(Path file) throws IOException {
+        int count = 0;
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(file)) {
+            for (Path entry : stream) {
+                count++;
+                count += deletePath(entry);
+            }
+        }
+        Files.delete(file);
+        return count;
     }
 }
