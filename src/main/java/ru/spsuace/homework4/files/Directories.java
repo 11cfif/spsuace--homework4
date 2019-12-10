37a330b1db2e8948023b04ef26e83c8a0b6e1ba3
16000190 <nikki-vlad.mail@mail.ru>
2019-12-10 13:14:15 +0300

    Метод File

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,7 +1,11 @@
 package ru.spsuace.homework4.files;
 
-public class Directories {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
+public class Directories {
 
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
@@ -9,14 +13,41 @@ public class Directories {
      * Если директории по существующему пути нет, то возвращаем 0.
      * Написать двумя способами. С использованием File
      */
-    public static int removeWithFile(String path) {
-        return 0;
-    }
 
+    public static int removeWithFile(String path) {
+
+        int count = 0;
+        File dir1 = new File(path);
+            if (!dir1.exists()) {
+            return count;
+            }
+        File[] listFiles = dir1.listFiles();
+        for (int i = 0; i < listFiles.length; i++) {
+            if (listFiles[i].isFile()) {
+                listFiles[i].delete();
+                count += 1;
+            }
+            if (listFiles[i].isDirectory()) {
+                count += removeWithFile(listFiles[i].getPath());
+            }
+        }
+        dir1.delete();
+        count += 1;
+        return count;
+    }
+}
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
-    }
-}
+   // public static int removeWithPath(String path) {
+// Path start = Paths.get(path);
+// DeleterVisitor deletePaths = new DeleterVisitor();
+// try {
+// Files.walkFileTree(start, deletePaths);
+// } catch (IOException exception) {
+// }
+//
+// return deletePaths.countFilesAndDirectories;
+// }
+
+
