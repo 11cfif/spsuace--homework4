4602a53e91908d53302be65608c6d51858ff560c
Yana <yanaaaaaa1998@mail.ru>
2019-12-10 23:14:08 +0300

    directories(file)+exception

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,4 +1,9 @@
 package ru.spsuace.homework4.files;
+import java.io.File;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Directories {
 
@@ -10,8 +15,33 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+
+        int countFiles = 0;
+        final File mainFile = new File(path);
+
+        if (mainFile.exists() && mainFile.isDirectory()) {
+            File[] allFile = mainFile.listFiles();
+            if (allFile != null) {
+                countFiles += 1;
+            } else {
+                for (int i = 0; i < allFile.length; i++) {
+                    if (allFile[i].isFile()) {
+                        allFile[i].delete();
+                        countFiles += 1;
+                    }
+                    if (allFile[i].isDirectory()) {
+                        countFiles += removeWithFile(allFile[i].getPath());
+                    }
+                }
+            }
+           mainFile.delete();
+            countFiles+=1;
+        }else{
+                return 0;
+            }
+        return countFiles;
     }
+
 
     /**
      * С использованием Path
