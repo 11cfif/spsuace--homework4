cc640722b4f9782570395d4a9518e5f2d7644374
Анастасия <zimnitskayasakyra@mail.ru>
2019-12-11 00:34:33 +0300

    Directories

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,5 +1,13 @@
 package ru.spsuace.homework4.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 public class Directories {
 
 
@@ -10,13 +18,44 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        int countRemoved = 0;
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (Objects.requireNonNull(files).length > 0) {
+                    for (File f : files) {
+                        countRemoved += removeWithFile(f.getPath());
+                    }
+                }
+            }
+            return countRemoved + ((file.delete()) ? 1 : 0);
+        } else {
+            return 0;
+        }
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
+        int countRemoved = 0;
+        Path file = Paths.get(path);
+        if (file.toFile().exists()) {
+            try {
+                if (Files.isDirectory(file)) {
+                    DirectoryStream<Path> directoryStream = Files.newDirectoryStream(file);
+                    for (Path f : directoryStream) {
+                        countRemoved += removeWithPath(f.toString());
+                    }
+                    directoryStream.close();
+                }
+                Files.delete(file);
+                return countRemoved + 1;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return 0;
     }
 }
