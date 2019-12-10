cf4dd5a01f6a9430df58b8c2d15792c34cc6817b
Anna <anya.lakeeva1@mail.ru>
2019-12-10 22:37:20 +0300

    Directory

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,8 +1,8 @@
 package ru.spsuace.homework4.files;
 
+import java.io.File;
+
 public class Directories {
-
-
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
      * Метод должен возвращать количество удаленных файла и директорий.
@@ -10,7 +10,32 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        int countDirectories = 0;
+        File Directory = new File(path);
+        if (!Directory.exists()) {
+            return countDirectories;
+        } else {
+            if (Directory.isDirectory()) {
+                File[] DirectoryFiles = Directory.listFiles();
+                if (DirectoryFiles != null) {
+                    for (File currentFile : DirectoryFiles) {
+                        if (currentFile.isFile()) {
+                            currentFile.delete();
+                            countDirectories += 1;
+                        } else if (currentFile.isDirectory()) {
+                            countDirectories += removeWithFile(currentFile.getPath());
+                        }
+                    }
+                }
+                Directory.delete();
+                countDirectories += 1;
+            }
+            if (Directory.isFile()) {
+                Directory.delete();
+                countDirectories += 1;
+            }
+        }
+        return countDirectories;
     }
 
     /**
