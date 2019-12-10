5d073f73a76cea00cb3aecdbf2ca76b3474b6d24
semen3325 <semen.ganzhara@gmail.com>
2019-12-10 19:51:23 +0300

    1

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,5 +1,7 @@
 package ru.spsuace.homework4.files;
 
+import java.io.File;
+
 public class Directories {
 
 
@@ -10,7 +12,32 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File folder = new File(path);
+        int counter = 0;
+
+        if (!folder.isDirectory()) {
+            if (folder.isFile()) {
+                folder.delete();
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        for (File item : folder.listFiles()) {
+            if (item == null) {
+                break;
+            }
+            if (item.isDirectory()) {
+                counter += removeWithFile(item.getPath());
+            } else {
+                item.delete();
+                counter++;
+            }
+        }
+        folder.delete();
+        counter++;
+
+        return counter;
     }
 
     /**
