04976a9851c5a9f051fbfb365f3823b6cb0668ba
Анастасия <zimnitskayasakyra@mail.ru>
2019-12-11 00:43:16 +0300

    remove unused import

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,7 +1,6 @@
 package ru.spsuace.homework4.files;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
