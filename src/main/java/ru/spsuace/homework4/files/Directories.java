a7fc0baf3a749866e64b316330368e0c35cb0124
Bfl4t <boitsov-iliya@yandex.ru>
2019-12-17 14:02:35 +0300

    Directories

--- src/main/java/ru/spsuace/homework4/files/Directories.java
+++ src/main/java/ru/spsuace/homework4/files/Directories.java
@@ -1,5 +1,14 @@
 package ru.spsuace.homework4.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +19,53 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File current_file = new File(path);
+        if (!current_file.exists()) {
+            return 0;
+        }
+
+        int count = 1;
+        if (current_file.isDirectory()) {
+            for (File file : current_file.listFiles()) {
+                count += removeWithFile(file.getPath());
+            }
+        }
+        current_file.delete();
+
+        return count;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        Path current_path = Paths.get(path);
+        if (!Files.exists(current_path)) {
+            return 0;
+        }
+
+        long count = 0;
+
+        try {
+            count = Files.walk(current_path).count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Files.walk(current_path)
+                    .sorted(Comparator.reverseOrder())
+                    .forEach(x -> {
+                        try {
+                            Files.delete(x);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return (int) count;
     }
 }
