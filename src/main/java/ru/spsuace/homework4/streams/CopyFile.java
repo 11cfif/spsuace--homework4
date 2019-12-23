853a16a8f82413f5d2de6dece987bdade9c90265
Dmitry <drujkin.dmitry@yandex.ru>
2019-12-23 21:18:18 +0300

    copyBigFile

--- src/main/java/ru/spsuace/homework4/streams/CopyFile.java
+++ src/main/java/ru/spsuace/homework4/streams/CopyFile.java
@@ -1,6 +1,10 @@
 package ru.spsuace.homework4.streams;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class CopyFile {
 
@@ -8,12 +12,28 @@ public class CopyFile {
      * Реализовать копирование больших файлов через стримы.
      */
     public static void copyFile(String pathFrom, String pathTo) throws IOException {
-    }
+//        File out = new File(pathFrom);
+        File in = new File(pathTo);
+            Path inf = Paths.get(pathTo);
+            Files.createDirectories(inf.getParent());
+            BufferedInputStream bufferInputStream = new BufferedInputStream(new FileInputStream(pathFrom));
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(pathTo));
+            try {
+                byte[] buff = new byte[1024];
+                int len;
+                while ((len = bufferInputStream.read(buff)) > 0) {
+                    bufferedOutputStream.write(buff, 0, len);
+                }
+            } finally {
+                bufferInputStream.close();
+                bufferedOutputStream.close();
+            }
+        }
 
 
     /**
      * Реализовать копирование из одной директории в другую
      */
-    public static void copyDir(String pathFrom, String pathTo) {
+    public static void copyDir(String pathFrom, String pathTo) throws IOException {
     }
 }
